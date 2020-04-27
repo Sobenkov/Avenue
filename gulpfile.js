31c1287e1595687ff8dc26fbd465e31a64a19583
Sobenkov <Sobenkov95@yandex.ru>
2020-04-27 13:13:59 +0300

    avenue_fasion 01

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,53 @@
-function defaultSomeTask(done) {
-   console.log("work");
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let autoprefixer = require('gulp-autoprefixer');
+let sourcemaps = require('gulp-sourcemaps');
+let browserSync = require('browser-sync').create();
+
+
+function cssStyle(done) {
+
+   gulp.src('./scss/**/*.scss')
+      .pipe(sourcemaps.init())
+      .pipe(sass({
+         errorLogToConsole: true,
+         outputStyle: 'compressed'
+      }))
+      .on('error', console.error.bind(console))
+      .pipe(autoprefixer({
+         cascade: false
+      }))
+      .pipe(sourcemaps.write('./css/style.css.map'))
+      .pipe(gulp.dest('css/'))
+      .pipe(browserSync.stream());
+
    done();
 }
 
-exports.default = defaultSomeTask;
+function watchStyle() {
+
+   gulp.watch('./scss/**/*', cssStyle);
+   gulp.watch('./**/*.html', browserReload);
+   gulp.watch('./**/*.php', browserReload);
+}
+
+function Sync(done) {
+
+   browserSync.init({
+      server: {
+         baseDir: './'
+      },
+      port: 3000
+   });
+
+   done();
+}
+
+function browserReload(done) {
+
+   browserSync.reload();
+
+   done();
+}
+
+gulp.task('default', gulp.parallel(Sync, watchStyle));
