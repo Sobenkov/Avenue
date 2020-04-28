b2717edd7c91936631d310d4ee00d8dd688361df
Sobenkov <Sobenkov95@yandex.ru>
2020-04-28 12:57:34 +0300

    Avenue 03

--- index.php
+++ index.php
@@ -4,13 +4,14 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Avenue Fasion</title>
+   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700|Roboto:400,700&display=swap&subset=cyrillic,cyrillic-ext" rel="stylesheet">
    <link rel="stylesheet" href="css/style.css"> 
 </head>
 <!-- -------------------------body-------------------------- -->
 <body>
    <!-- -------------------------header------------------------ -->
    <header>
-      <ul>
+      <ul class="header__auth">
          <li><a href="#">Зарегистрироваться</a></li>
          <li><a href="#">Войти</a></li>
          <li><a href="#">*КОРЗИНА*</a></li>
@@ -23,6 +24,7 @@
             <li>Фирменное</li>
             <li>Ближайшие магазины</li>
             <li>Книга образов</li>
+            <input type="search" placeholder="Найти...">
          </ul>
       </nav>
    </header>
