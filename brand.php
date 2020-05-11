3535719d2b1d6170fdaaaf6efa73fa9d95481cc2
Sobenkov <Sobenkov95@yandex.ru>
2020-05-11 13:25:57 +0300

    11.05. Avenue 02

--- brand.php
+++ brand.php
@@ -6,11 +6,27 @@ include "header.php"
 
 <div class="container">
    <div class="brand">
-      <div class="brand__left">
-         тест
+      <div class="brand__block">
+         <img src="/img/REPLACE4.png" alt="">
+         <img src="/img/REPLACE5.png" alt="">
+         <div class="brand__text">
+            <h3>Текст</h3>
+            <h4>Текст текст</h4>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident, consequatur temporibus animi doloribus quibusdam explicabo commodi accusamus cumque aperiam saepe quo quisquam voluptas labore, similique, tenetur debitis placeat quas et itaque nostrum sapiente est. Exercitationem voluptatibus facere officiis recusandae dicta!</p>
+         </div>
       </div>
-      <div class="brand__lright">
-         тест
+      <div class="brand__block">
+         <div class="brand__text">
+            <h3>Текст</h3>
+            <h4>Текст текст</h4>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident, consequatur temporibus animi doloribus quibusdam explicabo commodi accusamus cumque aperiam saepe quo quisquam voluptas labore, similique, tenetur debitis placeat quas et itaque nostrum sapiente est. Exercitationem voluptatibus facere officiis recusandae dicta!</p>
+         </div>
+         <img src="/img/REPLACE6.png" alt="">
+         <div class="brand__text">
+            <h3>Текст</h3>
+            <h4>Текст текст</h4>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident, consequatur temporibus animi doloribus quibusdam explicabo commodi accusamus cumque aperiam saepe quo quisquam voluptas labore, similique, tenetur debitis placeat quas et itaque nostrum sapiente est. Exercitationem voluptatibus facere officiis recusandae dicta!</p>
+         </div>
       </div>
    </div>
 </div>
